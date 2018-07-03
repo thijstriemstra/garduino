46f9449790a61090293c22a8ad181e1356920f1b
Thijs Triemstra <info@collab.nl>
2018-07-03 02:33:19 +0200

    add configparser

--- pygarden/util.py
+++ pygarden/util.py
@@ -4,7 +4,7 @@ Utilities.
 
 
 def get_config(fname):
-    from configparser.ConfigParser import ConfigParser
+    from pygarden.configparser.ConfigParser import ConfigParser
 
     cfg = ConfigParser()
     cfg.read(fname)
