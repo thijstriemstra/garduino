8a2972c9887be280b241c4ce2458bc6407f5f26a
Thijs Triemstra <info@collab.nl>
2018-07-24 15:30:13 +0200

    macos fix

--- tools/shared.sh
+++ tools/shared.sh
@@ -1,11 +1,13 @@
-# OSX
-export PORT=/dev/tty.wchusbserial1420
-# Linux
 export PORT=/dev/ttyUSB0
-
 export BAUD=115200
 export DELAY=0
 export CHIP=esp32
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    PORT=/dev/tty.wchusbserial1420
+    # Fix for macOS users' "Could not enter raw repl"; try 2.0 and lower from there:
+    DELAY=0.5
+fi
 
 echo "-------------------------------------"
 echo "Using "$PORT "(baudrate: "$BAUD")"
