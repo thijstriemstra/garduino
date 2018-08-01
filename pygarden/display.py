f512993beb2e34b507eadc1e4300599ac87b3bab
Thijs Triemstra <info@collab.nl>
2018-08-01 02:50:37 +0200

    update display

--- pygarden/display.py
+++ pygarden/display.py
@@ -1,7 +1,7 @@
 import logging
 from machine import Pin
 
-from pygarden.lib.tm1637 import TM1637
+from pygarden.lib import tm1637
 
 
 logger = logging.getLogger(__name__)
@@ -12,7 +12,7 @@ class TM1637Display(object):
     Quad 7-segment LED module.
     """
     def __init__(self, clk_pin, dio_pin):
-        self.display = TM1637(clk=Pin(clk_pin), dio=Pin(dio_pin))
+        self.display = tm1637.TM1637(clk=Pin(clk_pin), dio=Pin(dio_pin))
         self.clear()
 
     def show(self, msg):
@@ -22,4 +22,16 @@ class TM1637Display(object):
         self.display.temperature(temp)
 
     def clear(self):
+        """
+        Turn all LEDS off.
+        """
         self.display.write([0, 0, 0, 0])
+
+    def brightness(self, level):
+        self.display.brightness(level)
+
+    def all_chars(self):
+        """
+        Scroll all available characters.
+        """
+        self.display.scroll(list(tm1637._SEGMENTS))
