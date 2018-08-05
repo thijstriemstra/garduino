3b10a74f4cc985e02a02b5e46e252a2e40bfbda2
Thijs Triemstra <info@collab.nl>
2018-08-06 01:01:12 +0200

    add delay param

--- pygarden/display.py
+++ pygarden/display.py
@@ -12,7 +12,10 @@ class TM1637Display(object):
     Quad 7-segment LED module.
     """
     def __init__(self, clk_pin, dio_pin):
-        self.display = tm1637.TM1637(clk=Pin(clk_pin), dio=Pin(dio_pin))
+        self.display = tm1637.TM1637(
+            clk=Pin(clk_pin),
+            dio=Pin(dio_pin)
+        )
         self.clear()
 
     def show(self, msg):
@@ -21,34 +24,44 @@ class TM1637Display(object):
         """
         self.display.show(msg)
 
-    def temperature(self, temp):
+    def temperature(self, temp, delay=250):
         """
         Display temperature in celcius.
 
+        :param temp: Temperature (in degrees celsius).
         :type temp: int
         """
-        self.display.temperature(temp)
+        if (temp > 99) or (temp < -9):
+            # out of screen bounds so scroll
+            self.scroll('{:.0f}*C'.format(temp), delay=delay)
+        else:
+            self.display.temperature(temp)
 
     def clear(self):
         """
-        Turn all LEDS off.
+        Clear display.
         """
         self.display.write([0, 0, 0, 0])
 
     def brightness(self, level):
         """
         Set the display brightness 0-7.
+
+        :type level: int
         """
         self.display.brightness(level)
 
-    def scroll(self, msg):
+    def scroll(self, msg, delay=250):
         """
         Scroll a message.
+
+        :param msg: Messages to scroll.
+        :type msg: str
         """
-        self.display.scroll(msg)
+        self.display.scroll(msg, delay=delay)
 
     def all_chars(self):
         """
-        Scroll all available characters.
+        Scroll through all available characters.
         """
         self.scroll(list(tm1637._SEGMENTS))
