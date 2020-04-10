37cb4afb1fac848c454a535c930f31c340d8fad8
Thijs Triemstra <info@collab.nl>
2020-04-10 22:24:57 +0200

    add more sensors

--- arduino/pygarden/include/PyGarden.h
+++ arduino/pygarden/include/PyGarden.h
@@ -5,6 +5,9 @@
 #define PyGarden_h
 
 #include "Arduino.h"
+#include <YL83_RainSensor.h>
+#include <FC28_SoilSensor.h>
+#include <DS18B20_TemperatureSensors.h>
 
 #define VERSION "1.0.0"
 
@@ -14,6 +17,12 @@ class PyGarden
     PyGarden();
     void begin();
     void loop();
+
+  private:
+    YL83_RainSensor* _rain;
+    FC28_SoilSensor* _soil1;
+    FC28_SoilSensor* _soil2;
+    DS18B20_TemperatureSensors* _temperature;
 };
 
 #endif
--- arduino/pygarden/src/PyGarden.cpp
+++ arduino/pygarden/src/PyGarden.cpp
@@ -6,21 +6,25 @@
 #include "PyGarden.h"
 
 PyGarden::PyGarden() {
-  /*
-  _led = new LED(2, LOW);
-  _btn = new Button(3);
-  _switch = new Switch(4);
-  _display = new TM1637_LCDDisplay(5, 6);
-  _temperature = new DS18B20_TemperatureSensors(12);
-  _distance = new HCSR04_DistanceSensor(10, 11);
-  */
+  _rain = new YL83_RainSensor(RainSensorPin);
+  _soil1 = new FC28_SoilSensor(SoilSensor1Pin, SoilSensor1Dry, SoilSensor1Wet);
+  _soil2 = new FC28_SoilSensor(SoilSensor2Pin, SoilSensor2Dry, SoilSensor2Wet);
+  _temperature = new DS18B20_TemperatureSensors(TemperatureSensorsPin);
 }
 
 void PyGarden::begin() {
   // start serial connection
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
+  _rain->begin();
+  _soil1->begin();
+  _soil2->begin();
+  _temperature->begin();
 }
 
 void PyGarden::loop() {
+  _rain->loop();
+  _soil1->loop();
+  _soil2->loop();
+  _temperature->loop();
 }
