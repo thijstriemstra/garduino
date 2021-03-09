ec7b1e1586dc0936ccfef688f9effd42727ec6b0
Thijs Triemstra <info@collab.nl>
2021-03-09 02:27:55 +0100

    add multiplexers

--- include/Garduinov3.h
+++ include/Garduinov3.h
@@ -15,48 +15,51 @@
 #include <WateringTask.h>
 #include <PowerManagement.h>
 #include <SSD1306_OLEDDisplay.h>
+#include <MultiPlexer_MCP3008.h>
+#include <MultiPlexer_TCA9548A.h>
 
-class Garduinov3
-{
-public:
-    Garduinov3();
-    void begin();
-    void loop();
-    void openValve();
-    void closeValve();
-    void toggleValve();
-    void checkWatering();
-    void startManualMode();
-    void sleep(bool forced = false);
+class Garduinov3 {
+  public:
+      Garduinov3();
+      void begin();
+      void loop();
+      void openValve();
+      void closeValve();
+      void toggleValve();
+      void checkWatering();
+      void startManualMode();
+      void sleep(bool forced = false);
 
-    bool started = false;
-    bool connected = false;
+      bool started = false;
+      bool connected = false;
 
-private:
-    IOT *_iot;
-    Sensors *_sensors;
-    Controls *_controls;
-    SystemClock *_clock;
-    PowerManagement *_power;
-    WateringTask *_wateringTask;
-    SSD1306_OLEDDisplay *_display;
+  private:
+      IOT *_iot;
+      Sensors *_sensors;
+      Controls *_controls;
+      SystemClock *_clock;
+      PowerManagement *_power;
+      MultiPlexer_MCP3008 *_adc;
+      MultiPlexer_TCA9548A *_ic2;
+      WateringTask *_wateringTask;
+      SSD1306_OLEDDisplay *_display;
 
-    bool _manualMode = false;
-    int _totalReadings = 15;
-    const char *_namespace = "garduino";
-    const char *_version = "3.0.0";
+      bool _manualMode = false;
+      int _totalReadings = 15;
+      const char *_namespace = "garduino";
+      const char *_version = "3.0.0";
 
-    // callbacks
-    void onSystemWakeup();
-    void onPowerButtonPush();
-    void onManualButtonPush();
-    void onConnectionReady();
-    void onConnectionClosed();
-    void onConnectionFailed();
-    void onPublishReady();
-    void onWateringReady();
-    void onValveOpen();
-    void onValveClosed();
+      // callbacks
+      void onSystemWakeup();
+      void onPowerButtonPush();
+      void onManualButtonPush();
+      void onConnectionReady();
+      void onConnectionClosed();
+      void onConnectionFailed();
+      void onPublishReady();
+      void onWateringReady();
+      void onValveOpen();
+      void onValveClosed();
 };
 
 #endif
