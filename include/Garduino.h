dd708095b9835febebcb85bc3963ea5d4e0ce73e
Thijs Triemstra <info@collab.nl>
2021-05-19 02:06:08 +0200

    merge to main class

--- include/Garduino.h
+++ include/Garduino.h
@@ -1,3 +1,6 @@
+/*  Copyright (c) 2020-2021, Collab
+ *  All rights reserved
+*/
 /*
   Garduino.h - Library for monitoring a garden.
 */
@@ -9,60 +12,56 @@
 #include <Method.h>
 
 #include <IOT.h>
-#include <LED.h>
-#include <Button.h>
+#include <Utils.h>
+#include <Controls.h>
 #include <Sensors.h>
 #include <SystemClock.h>
 #include <WateringTask.h>
 #include <PowerManagement.h>
-#include <SSD1306_OLEDDisplay.h>
+#include <MultiPlexer_TCA9548A.h>
+#include <SSD1306_OLEDDisplay_Mux.h>
 
-class Garduino
-{
+class Garduino {
   public:
-    Garduino();
-    void begin();
-    void loop();
-    void openValve();
-    void closeValve();
-    void toggleValve();
-    void checkWatering();
-    void startManualMode();
-    void sleep(bool forced = false);
+      Garduino();
+      void begin();
+      void loop();
+      void openValve();
+      void closeValve();
+      void toggleValve();
+      void checkWatering();
+      void startManualMode();
+      void sleep(bool forced = false);
 
-    bool started = false;
-    bool connected = false;
+      bool started = false;
+      bool connected = false;
 
   private:
-    IOT* _iot;
-    LED* _powerLED;
-    LED* _manualLED;
-    LED* _networkLED;
-    Sensors* _sensors;
-    Button* _powerBtn;
-    Button* _manualBtn;
-    SystemClock* _clock;
-    PowerManagement* _power;
-    WateringTask* _wateringTask;
-    //ThreadController* _scheduler;
-    SSD1306_OLEDDisplay* _display;
+      IOT *_iot;
+      Sensors* _sensors;
+      Controls* _controls;
+      SystemClock* _clock;
+      PowerManagement* _power;
+      MultiPlexer_TCA9548A* _i2c;
+      WateringTask* _wateringTask;
+      SSD1306_OLEDDisplay_Mux* _display;
 
-    bool _manualMode = false;
-    int _totalReadings = 15;
-    const char* _namespace = "garduino";
-    const char* _version = "2.4.2";
+      bool _manualMode = false;
+      int _totalReadings = 15;
+      const char *_namespace = "garduino";
+      const char *_version = "3.0.0";
 
-    // callbacks
-    void onSystemWakeup();
-    void onPowerButtonPush();
-    void onManualButtonPush();
-    void onConnectionReady();
-    void onConnectionClosed();
-    void onConnectionFailed();
-    void onPublishReady();
-    void onWateringReady();
-    void onValveOpen();
-    void onValveClosed();
+      // callbacks
+      void onSystemWakeup();
+      void onPowerButtonPush();
+      void onManualButtonPush();
+      void onConnectionReady();
+      void onConnectionClosed();
+      void onConnectionFailed();
+      void onPublishReady();
+      void onWateringReady();
+      void onValveOpen();
+      void onValveClosed();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 * All rights reserved
 */
 
-#include "Garduinov3.h"
+#include "Garduino.h"
 #define SERIAL_BAUD_RATE 115200
 
-Garduinov3 *app = new Garduinov3();
+Garduino *app = new Garduino();
 
 void setup() {
   // start serial connection
